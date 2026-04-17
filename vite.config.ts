9d8be778b5019493a19dd5b02bf334be4e08822c
mariasuitedigital-dotcom <mariasuitedigital@gmail.com>
2026-04-16 19:34:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // ESTO ES LO QUE SOLUCIONARA LA PANTALLA EN BLANCO
+  base: './', 
+  
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
